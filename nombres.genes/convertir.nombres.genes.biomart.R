8ea4b5393c07fdd81b4158650bfc2e929e64dff6
jbzt <juanantoniobizzotto@gmail.com>
2024-01-04 10:08:48 -0300

    puse primer script para cambiar nombres de genes

---
+++ nombres.genes/convertir.nombres.genes.biomart.R
@@ -0,0 +1,25 @@
+# script para convertir nombres de genes, o IDs, e intercambiarlos usando biomart
+
+library(biomaRt)
+
+# base de datos a usar
+ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
+
+# ids de entrada (a modo de ejemplo)
+ids.interes=c("ENST00000395956.7", "ENST00000676005.1")
+
+# que tipo de ids estoy usando de entrada?
+ensembl@filters[,1] #aca veo todas las opciones, me quedo con la que me intresa
+ensembl@filters[56,1] # estas son las que me interesan en este eje mplo
+
+# que tipo de info quiero que me devuelva
+ensembl@attributes[,1]
+ensembl@attributes[63,1] #encontre la que me intresa
+
+# esto es solo para ver si funciona la herramienta
+# como pensarlo: yo te doy estos VALUES, anda a la tabla MART, fijate en la columna que puse en FILTERS,  y dame la data de las columnas ATRIBUTES
+
+getBM(attributes = 'hgnc_symbol', # caracteristicas que quiero que me devuelva
+      filters = 'ensembl_transcript_id_version', #caracteristica con la que voy a comparar mi lista de genes
+      values = ids.interes, # lista de genes de interes
+      mart = ensembl) # datos a usar
