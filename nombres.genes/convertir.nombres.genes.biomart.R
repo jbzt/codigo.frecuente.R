49a434ec6a7b371651f74e77f34a508dc1c860db
asabater00 <79541544+asabater00@users.noreply.github.com>
2024-01-04 10:40:35 -0300

    Update convertir.nombres.genes.biomart.R
    Sume un comentario pavo al final para probar

--- nombres.genes/convertir.nombres.genes.biomart.R
+++ nombres.genes/convertir.nombres.genes.biomart.R
@@ -10,7 +10,7 @@ ids.interes=c("ENST00000395956.7", "ENST00000676005.1")
 
 # que tipo de ids estoy usando de entrada?
 ensembl@filters[,1] #aca veo todas las opciones, me quedo con la que me intresa
-ensembl@filters[56,1] # estas son las que me interesan en este eje mplo
+ensembl@filters[56,1] # estas son las que me interesan en este ejemplo
 
 # que tipo de info quiero que me devuelva
 ensembl@attributes[,1]
@@ -23,3 +23,5 @@ getBM(attributes = 'hgnc_symbol', # caracteristicas que quiero que me devuelva
       filters = 'ensembl_transcript_id_version', #caracteristica con la que voy a comparar mi lista de genes
       values = ids.interes, # lista de genes de interes
       mart = ensembl) # datos a usar
+
+#sumo un comentario de prueba para probar la herramienta <3 Agus
